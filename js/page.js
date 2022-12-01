d210246fd420ba02a2c62a579e4f5932a69c95ac
aschieff <schiefar@clarkson.edu>
2022-12-01 11:09:20 -0500

    Made Game Work

--- js/page.js
+++ js/page.js
@@ -1,1 +1,43 @@
-
+function choice(x) {
+    sessionStorage.playerchoice=x;
+var y =Math.ceil(Math.random()*3);
+if (y==1){
+    sessionStorage.oppchoice="rock";
+}
+else if (y==2){
+    sessionStorage.oppchoice="scissors";
+}
+else if (y==3){
+    sessionStorage.oppchoice="paper";
+}
+if ((sessionStorage.playerchoice == "rock") && (sessionStorage.oppchoice == "paper")) {
+    sessionStorage.result = "lose";
+}
+else if ((sessionStorage.playerchoice == "rock") && (sessionStorage.oppchoice == "scissors")){
+    sessionStorage.result = "win";
+}
+else if ((sessionStorage.playerchoice == "paper") && (sessionStorage.oppchoice == "scissors")){
+    sessionStorage.result = "lose";
+}
+else if ((sessionStorage.playerchoice == "paper") && (sessionStorage.oppchoice == "rock")) {
+    sessionStorage.result = "win";
+}
+else if ((sessionStorage.playerchoice == "scissors") && (sessionStorage.oppchoice == "rock")){
+    sessionStorage.result = "lose";
+}
+else if ((sessionStorage.playerchoice == "scissors") && (sessionStorage.oppchoice == "paper")){
+    sessionStorage.result = "win";
+}else if ((sessionStorage.playerchoice == sessionStorage.oppchoice)){
+    sessionStorage.result = "tie"
+}
+document.getElementById("o-img").src = "svg/" + sessionStorage.oppchoice + ".svg";
+if (sessionStorage.result=="win"){
+    document.getElementById("result").innerHTML="Congrats you won!";
+}
+else if (sessionStorage.result=="lose"){
+    document.getElementById("result").innerHTML="You lose. Better luck next time.";
+}
+else if (sessionStorage.result=="tie"){
+    document.getElementById("result").innerHTML="It's a tie. Try again.";
+}
+}
